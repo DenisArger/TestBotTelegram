40a3d1b95ae7ebc06c63a952bd37bf55b5879fc4
Denis Arger <den.arger@gmail.com>
2023-10-04 12:30:38 +0300

    fix: delete comment

--- sendMessage.js
+++ sendMessage.js
@@ -16,20 +16,3 @@ module.exports = async (chat_id, text) => {
     throw error;
   }
 };
-
-// async function sendMessage(chat_id, text) {
-//   try {
-//     await axios.post(
-//       `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-//       {
-//         chat_id,
-//         text,
-//       }
-//     );
-
-//     return true;
-//   } catch (error) {
-//     console.error("Error sending message:", error);
-//     throw error;
-//   }
-// }
