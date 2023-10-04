55e23fb595d4687cf231d3e5e64f6d3c664a1bf2
Denis Arger <den.arger@gmail.com>
2023-10-04 12:22:11 +0300

    add echo

--- sendMessage.js
+++ sendMessage.js
@@ -1,13 +1,35 @@
 const axios = require("axios").default;
 
 module.exports = async (chat_id, text) => {
-  await axios.post(
-    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-    {
-      chat_id,
-      text,
-    }
-  );
+  try {
+    await axios.post(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+      {
+        chat_id,
+        text,
+      }
+    );
 
-  return true;
+    return true;
+  } catch (error) {
+    console.error("Error sending message:", error);
+    throw error;
+  }
 };
+
+// async function sendMessage(chat_id, text) {
+//   try {
+//     await axios.post(
+//       `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+//       {
+//         chat_id,
+//         text,
+//       }
+//     );
+
+//     return true;
+//   } catch (error) {
+//     console.error("Error sending message:", error);
+//     throw error;
+//   }
+// }
