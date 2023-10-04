ddbb8ff072b7643eabd7ef2807db07eb8e04ba57
Denis Arger <den.arger@gmail.com>
2023-10-04 13:23:31 +0300

    fix: minimal

--- sendMessage.js
+++ sendMessage.js
@@ -1,18 +1,13 @@
 const axios = require("axios").default;
 
 module.exports = async (chat_id, text) => {
-  try {
-    await axios.post(
-      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-      {
-        chat_id,
-        text,
-      }
-    );
+  await axios.post(
+    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+    {
+      chat_id,
+      text,
+    }
+  );
 
-    return true;
-  } catch (error) {
-    console.error("Error sending message:", error);
-    throw error;
-  }
+  return true;
 };
