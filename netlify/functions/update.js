89f0913cc19609f5774b18ec34b0a5616ddf6816
Denis Arger <den.arger@gmail.com>
2023-10-04 12:30:57 +0300

    feat: add command welcome

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -4,8 +4,13 @@ exports.handler = async (event) => {
   try {
     const { message } = JSON.parse(event.body);
 
-    // Отправляем сообщение обратно с тем же текстом и chat_id
-    await sendMessage(message.chat.id, message.text);
+    if (message.text.startsWith("/start")) {
+      // Обработка команды /start и отправка приветственного сообщения
+      await sendWelcomeMessage(message.chat.id);
+    } else {
+      // В противном случае, отправка сообщения с тем же текстом
+      await sendMessage(message.chat.id, message.text);
+    }
 
     return { statusCode: 200 };
   } catch (error) {
@@ -16,3 +21,16 @@ exports.handler = async (event) => {
     };
   }
 };
+
+async function sendWelcomeMessage(chat_id) {
+  try {
+    // Текст приветственного сообщения
+    const welcomeText = "Добро пожаловать! Я ваш бот. Как я могу вам помочь?";
+
+    // Отправка приветственного сообщения
+    await sendMessage(chat_id, welcomeText);
+  } catch (error) {
+    console.error("Error sending welcome message:", error);
+    throw error;
+  }
+}
