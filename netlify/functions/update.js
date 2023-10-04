66c24e0a9417231dd1b281026209a039e31da9c0
Denis Arger <den.arger@gmail.com>
2023-10-04 13:06:49 +0300

    fix: echo

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,18 +1,11 @@
 const sendMessage = require("../../sendMessage");
-const notionApiKey = process.env.NOTION_API_KEY;
-const databaseId = process.env.NOTION_DATABASE_ID;
 
 exports.handler = async (event) => {
   try {
     const { message } = JSON.parse(event.body);
 
-    if (message.text.startsWith("/start")) {
-      // Обработка команды /start и отправка приветственного сообщения
-      await sendWelcomeMessage(message.chat.id);
-    } else {
-      // В противном случае, отправка сообщения с тем же текстом
-      await sendMessage(message.chat.id, message.text);
-    }
+    // Отправляем сообщение обратно с тем же текстом и chat_id
+    await sendMessage(message.chat.id, message.text);
 
     return { statusCode: 200 };
   } catch (error) {
@@ -23,16 +16,3 @@ exports.handler = async (event) => {
     };
   }
 };
-
-async function sendWelcomeMessage(chat_id) {
-  try {
-    // Текст приветственного сообщения
-    const welcomeText = "Добро пожаловать! Я ваш бот. Как я могу вам помочь?";
-
-    // Отправка приветственного сообщения
-    await sendMessage(chat_id, welcomeText);
-  } catch (error) {
-    console.error("Error sending welcome message:", error);
-    throw error;
-  }
-}
