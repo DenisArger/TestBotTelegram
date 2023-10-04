3bc86477c4bae8564df229b2063a4844460e9309
Denis Arger <den.arger@gmail.com>
2023-10-04 12:52:13 +0300

    feat: add check chat
    # Conflicts:
    #	netlify/functions/update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,7 +1,108 @@
 const sendMessage = require("../../sendMessage");
+const { Client } = require("@notionhq/client");
+
+const notionApiKey = process.env.NOTION_API_KEY;
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+// Инициализация клиента Notion API
+const notion = new Client({ auth: notionApiKey });
 
 exports.handler = async (event) => {
-  const { message } = JSON.parse(event.body);
-  await sendMessage(message.chat.id, "I got your message!");
-  return { statusCode: 200 };
+  try {
+    const { message } = JSON.parse(event.body);
+
+    if (!message || !message.chat || !message.text) {
+      // Обработка случая, когда не хватает необходимых полей
+      await sendMessage(message.chat?.id, "Неверный формат сообщения");
+      return { statusCode: 400 }; // Возвращаем статус код 400 для указания ошибки клиента
+    }
+
+    if (message.text.startsWith("/start")) {
+      // Обработка команды /start и отправка приветственного сообщения
+      await sendWelcomeMessage(message.chat.id);
+    } else if (message.text.startsWith("/notion")) {
+      // Обработка команды /notion и запись данных в Notion
+      const params = message.text.split(" ");
+      if (params.length === 2) {
+        const [, username] = params;
+        const name = message.from.first_name; // Имя пользователя из Telegram
+        const currentDate = new Date().toISOString().split("T")[0]; // Текущая дата в формате "гггг-мм-дд"
+        await addToDatabase(databaseId, username, name, true, currentDate);
+      } else {
+        await sendMessage(
+          message.chat.id,
+          "Неверное количество параметров. Используйте /notion username"
+        );
+      }
+    } else {
+      // В противном случае, отправка сообщения с тем же текстом
+      await sendMessage(message.chat.id, message.text);
+    }
+
+    return { statusCode: 200 };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
 };
+
+async function sendWelcomeMessage(chat_id) {
+  try {
+    // Текст приветственного сообщения
+    const welcomeText = "Добро пожаловать! Я ваш бот. Как я могу вам помочь?";
+
+    // Отправка приветственного сообщения
+    await sendMessage(chat_id, welcomeText);
+  } catch (error) {
+    console.error("Error sending welcome message:", error);
+    throw error;
+  }
+}
+
+// Функция для добавления записи в Notion
+async function addToDatabase(databaseId, username, name, status, date) {
+  try {
+    const response = await notion.pages.create({
+      parent: {
+        database_id: databaseId,
+      },
+      properties: {
+        ID: {
+          title: [
+            {
+              type: "text",
+              text: {
+                content: username,
+              },
+            },
+          ],
+        },
+        Name: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: name,
+              },
+            },
+          ],
+        },
+        Status: {
+          checkbox: status,
+        },
+        Date: {
+          date: {
+            start: date,
+          },
+        },
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.error(error.body);
+    throw error;
+  }
+}
