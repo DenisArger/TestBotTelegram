605e006fa8d0aadfb5110ca73ea68d386f17f000
Denis Arger <den.arger@gmail.com>
2023-10-04 13:11:51 +0300

    fix: base version

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,18 +1,7 @@
 const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
-  try {
-    const { message } = JSON.parse(event.body);
-
-    // Отправляем сообщение обратно с тем же текстом и chat_id
-    await sendMessage(message.chat.id, message.text);
-
-    return { statusCode: 200 };
-  } catch (error) {
-    console.error("Error:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Internal Server Error" }),
-    };
-  }
+  const { message } = JSON.parse(event.body);
+  await sendMessage(message.chat.id, "I got your message!");
+  return { statusCode: 200 };
 };
